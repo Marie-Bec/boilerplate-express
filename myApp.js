aeefaf24f0482e5cafba68e4a45eebc976171f58
Marie-Bec <131213242+Marie-Bec@users.noreply.github.com>
2023-04-20 13:50:18 +0000

    .env use challenge

--- myApp.js
+++ myApp.js
@@ -1,40 +1,17 @@
 let express = require('express');
 let app = express();
 
-// SERVE STATIC ASSETS (SEND THE CSS TO THE INDEX)
-app.use("/public", express.static(__dirname + '/public'));
-// SEND THE INDEX TO BE DISPLAYED
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
-// Serve JSON on a Specific Route
+app.use("/public", express.static(__dirname + '/public'));
 
-// app.get('/json', (req, res) => {
-//   res.json({
-//       message: "Hello json".toUpperCase();
-//     });
-// });
-
-
-
-
-
-
-// app.get('/json', (req, res) => { res.json(
-//   if (process.env.MESSAGE_STYLE === 'uppercase'){
-//   return {
-//         message: "Hello json".toUpperCase();
-//       }
-//   } else {
-//     return {
-//         message: "Hello json";
-//       })
-//   }
-// }
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+  if(process.env['MESSAGE_STYLE'] == 'uppercase'){
+    res.json({'message': 'HELLO JSON'})
+  } else {
+    res.json({'message': 'Hello json'})
+  }
+});
 
 
 
