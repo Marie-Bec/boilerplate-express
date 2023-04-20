c3c0b340dede133f0ae1dab2bf4c64041ec8ba1a
Marie-Bec <131213242+Marie-Bec@users.noreply.github.com>
2023-04-20 13:02:21 +0000

    app.get and app.use

--- myApp.js
+++ myApp.js
@@ -1,6 +1,34 @@
 let express = require('express');
 let app = express();
 
+// SERVE STATIC ASSETS (SEND THE CSS TO THE INDEX)
+app.use("/public", express.static(__dirname + '/public'));
+// SEND THE INDEX TO BE DISPLAYED
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+// Serve JSON on a Specific Route
+
+// app.get('/json', (req, res) => {
+//   res.json({
+//       message: "Hello json".toUpperCase();
+//     });
+// });
+
+
+
+
+
+
+// app.get('/json', (req, res) => { res.json(
+//   if (process.env.MESSAGE_STYLE === 'uppercase'){
+//   return {
+//         message: "Hello json".toUpperCase();
+//       }
+//   } else {
+//     return {
+//         message: "Hello json";
+//       })
+//   }
+// }
 
 
 
@@ -29,11 +57,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
