912ad64dc59ef6018d2c7b0fff3e52cdbc19c7e0
Marie-Bec <131213242+Marie-Bec@users.noreply.github.com>
2023-05-04 14:13:21 +0000

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -2,19 +2,28 @@ let express = require('express');
 let app = express();
 
 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({time: req.time});
+});
+
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.use("/public", express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-  if(process.env['MESSAGE_STYLE'] == 'uppercase'){
-    res.json({'message': 'HELLO JSON'})
+  if (process.env['MESSAGE_STYLE'] == 'uppercase') {
+    res.json({ 'message': 'HELLO JSON' })
   } else {
-    res.json({'message': 'Hello json'})
+    res.json({ 'message': 'Hello json' })
   }
 });
-
-
-
 
 
 
