4e8e204fefece2412889e875c9010ada847937c1
Marie-Bec <131213242+Marie-Bec@users.noreply.github.com>
2023-05-08 13:45:22 +0000

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -1,29 +1,42 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/now", (req, res, next) => {
-  req.time = new Date().toString();
-  next();
-}, (req, res) => {
-  res.send({time: req.time});
+app.post("/name", function(req, res) {
+  var fullName = req.body.first + " " + req.body.last;
+  res.json
+    ({ name: fullName });
 });
 
-app.use(function(req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
+// app.get("/:word/echo", (req, res) => {
+//   req.word = req.params.word;
+//   res.send({ echo: req.word });
+// });
+
+// app.get("/now", (req, res, next) => {
+//   req.time = new Date().toString();
+//   next();
+// }, (req, res) => {
+//   res.send({ time: req.time });
+// });
+
+// app.use(function(req, res, next) {
+//   console.log(req.method + " " + req.path + " - " + req.ip);
+//   next();
+// });
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.use("/public", express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => {
-  if (process.env['MESSAGE_STYLE'] == 'uppercase') {
-    res.json({ 'message': 'HELLO JSON' })
-  } else {
-    res.json({ 'message': 'Hello json' })
-  }
-});
+// app.get('/json', (req, res) => {
+//   if (process.env['MESSAGE_STYLE'] == 'uppercase') {
+//     res.json({ 'message': 'HELLO JSON' })
+//   } else {
+//     res.json({ 'message': 'Hello json' })
+//   }
+// });
 
 
 
